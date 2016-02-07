6f03359bd4bf3a43a0532cb5cc59f857f226e2c7
Mishel Velez <mishelv1@gmail.com>
2016-02-06 19:57:11 -0600

    Changes in the name and the message of the purchase mailer

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,6 +1,6 @@
 class PurchaseMailer < ActionMailer::Base
 	layout 'purchase_mailer'
-	default from: "One Month Commerce <commerce@onemonth.com>"
+	default from: "Dressmytrip <commerce@onemonth.com>"
 
 	def purchase_receipt purchase
 		@purchase = purchase
