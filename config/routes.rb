7c7d08ad091751e91d3fc8e5ea84a96b24991883
Mishel Velez <mishelv1@gmail.com>
2016-02-07 12:09:33 -0600

    Added activeAdmin and devise gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users, ActiveAdmin::Devise.config
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'pages#home'
 
   # Support stripe payments through charges
