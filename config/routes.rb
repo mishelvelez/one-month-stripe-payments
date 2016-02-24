d37236e39552fb8a00319378fbde896561f71ca8
Mishel Velez <mishelv1@gmail.com>
2016-02-23 22:53:05 -0600

    Installed activeAdmin and ran migrations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users, ActiveAdmin::Devise.config
-  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'pages#home'
 
