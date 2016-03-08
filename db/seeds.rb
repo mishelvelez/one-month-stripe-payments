dd93853b2027f2ed8d10c13b29d3e242ab9e9373
Mishel Velez <mishelv1@gmail.com>
2016-03-07 19:22:36 -0600

    Added growth hacking course to seeds file for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+growth_hacking = Product.create(title: "Growth Hacking", 
+	subtitle: "LA PLATAFORMA PARA QUE VIAJES SIN EQUIPAJE", author: "Mishel Velez", price: "4.99", sku: "GROHACK1", description: %{<p>No importa qué tan lejos viajes, ya no tendrás que cargar con ese pesado y molesto equipaje. Ahora puedes alquilar ropa en cualquier parte del mundo y recibirla en el lugar en que te hospedas.</p>
+
+            <p>bla bla bla bla quiero ser un website que te atraiga y te vuelva loco o loca.</p>
+            <p><strong>¿Cómo funciona?</strong></p>
+            <ul class="no-indent">
+                <li>Escribe la ciudad a la que vas a viajar</li>
+                <li>Elige la tienda en la que quieres alquilar tu ropa de invierno</li>
+                <li>Elige tus prendas y paga por Visa o Paypal</li>
+                <li>Recibe tus prendas en el hotel o apartamento que reservaste</li>
+            </ul>})
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
