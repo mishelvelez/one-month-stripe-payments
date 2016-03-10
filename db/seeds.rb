77d406b825db6500a72386cee535c0a2066901cb
Mishel Velez <mishelv1@gmail.com>
2016-03-10 08:29:42 -0600

    Implemented subscription based product GROHACK2

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,4 +23,22 @@ growth_hacking = Product.create(title: "Growth Hacking",
                 </p>
                 <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
 
+growth_hacking_monthly = Product.create(title: "Growth Hacking Monthly", 
+    subtitle: "Ongoing Course", author: "Mishel Velez", 
+    length: "6 months", author_image_name: "teacher-image.jpg", 
+    price: "4.99", sku: "GROHACK2", download_url: "https://s3.amazonaws.com/onemonthmishel/GIRL+COAT.png", details: "We will charge you monthly for unlimited access to our mentoring team", description: %{<p>No importa qué tan lejos viajes, ya no tendrás que cargar con ese pesado y molesto equipaje. Ahora puedes alquilar ropa en cualquier parte del mundo y recibirla en el lugar en que te hospedas.</p>
+
+            <p>In this series, Mishel Velez will going to teach you how Dressmytrip works.</p>
+            <p><strong>¿Cómo funciona?</strong></p>
+            <ul class="no-indent">
+                <li>Escribe la ciudad a la que vas a viajar</li>
+                <li>Elige la tienda en la que quieres alquilar tu ropa de invierno</li>
+                <li>Elige tus prendas y paga por Visa o Paypal</li>
+                <li>Recibe tus prendas en el hotel o apartamento que reservaste</li>
+            </ul>}, 
+        author_description: %{<p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+                <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
+                </p>
+                <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
