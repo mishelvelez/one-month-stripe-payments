2326c239d90bb807c6cba2d82bcafb98a3d37556
Mishel Velez <mishelv1@gmail.com>
2016-03-12 17:15:23 -0600

    Implemented product list page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
-  devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'pages#home'
 
   # Support stripe payments through charges
   resources :charges
 
+  # Route for showing products
+  resources :products, only: [:index]
+
   # Route for showing purchases
   resources :purchases, only: [:show]
-  
+
+   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,4 +25,38 @@ growth_hacking_monthly = Product.create(title: "Growth Hacking Monthly",
                 </p>
                 <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
 
+
+renting_monthly = Product.create(title: "Renting Monthly", 
+    subtitle: "Rent clothes", author: "Sada Flamenco", 
+    length: "6 months", author_image_name: "teacher-image.jpg", 
+    price: "4.99", sku: "RENTING2", download_url: "https://s3.amazonaws.com/onemonthmishel/GIRL+COAT.png", details: "We will charge you monthly for unlimited access to our mentoring team", description: %{<p>No importa qué tan lejos viajes, ya no tendrás que cargar con ese pesado y molesto equipaje. Ahora puedes alquilar ropa en cualquier parte del mundo y recibirla en el lugar en que te hospedas.</p>
+
+            <p>In this page, Sada Flamenco will going to talk about the importance of renting clothes.</p>
+            <p><strong>¿Cómo funciona?</strong></p>
+            <ul class="no-indent">
+                <li>How travel without luggage</li>
+                <li>Seven secrets of light traveling</li>
+            </ul>}, 
+        author_description: %{<p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+                <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
+                </p>
+                <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
+
+
+traveling_monthly = Product.create(title: "Traveling Monthly", 
+    subtitle: "traveling clothes", author: "Sada Flamenco", 
+    length: "6 months", author_image_name: "teacher-image.jpg", 
+    price: "4.99", sku: "TRAVELING2", download_url: "https://s3.amazonaws.com/onemonthmishel/GIRL+COAT.png", details: "We will charge you monthly for unlimited access to our mentoring team", description: %{<p>No importa qué tan lejos viajes, ya no tendrás que cargar con ese pesado y molesto equipaje. Ahora puedes alquilar ropa en cualquier parte del mundo y recibirla en el lugar en que te hospedas.</p>
+
+            <p>In this page, Sada Flamenco will going to talk about the importance of renting clothes.</p>
+            <p><strong>¿Cómo funciona?</strong></p>
+            <ul class="no-indent">
+                <li>How choose the best coat to travel</li>
+                <li>Show your shoulders, now you can travel without luggage</li>
+            </ul>}, 
+        author_description: %{<p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+                <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
+                </p>
+                <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
